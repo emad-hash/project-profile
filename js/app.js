de768563cae51d1566a65ab925bc73f07bac6bbe
emad-hash <emadhashash76@gmail.com>
2023-02-20 13:34:19 +0300

    emad223

--- js/app.js
+++ js/app.js
@@ -1,12 +1,33 @@
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-      },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
+const navMenu = document.getElementById('nav-menu'),
+   navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+      // ======= show menu
+      if(navToggle){
+        navToggle.addEventListener('click', () => {
+          navMenu.classList.add('show-menu')
+        })
+      }
+      // =======  menu hidden / close
+      if(navClose){
+        navClose.addEventListener('click', () => {
+          navMenu.classList.remove('show-menu')
+        })
+      }
+      // =======  remove mune mobile
+      const navLinks = document.querySelectorAll(".nav-link")
+
+      function linkActive() {
+        const navMenu = document.getElementById("nav-menu")
+        // when we click on each nav link , we remove the show menu class
+        navMenu.classList.remove("show-menu")
+      }
+      navLinks.forEach(n => n.addEventListener('click', linkActive))
+      // change background header
+      function scrollHeader(){
+        const header = document.getElementById("header")
+        // when the scroll is greater than 80 viewprot height , add the class scroll header to the tage header
+        if(this.scrollY >= 80 ) header.classList.add("scroll-header");
+        else header.classList.remove("scroll-header")
+      }
+      window.addEventListener("scroll", scrollHeader)
